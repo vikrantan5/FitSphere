f20b792f0b592b6eb99b1782edb4e1acbb9b7ea9
Vikrant Singh <vikrantsinghan5@gmail.com>
2026-02-18 19:08:00 +0530

    cnbfdgh

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
+import { AuthProvider } from "./contexts/AuthContext";   // 👈 ADD THIS
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <AuthProvider>      {/* 👈 WRAP APP */}
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 );
